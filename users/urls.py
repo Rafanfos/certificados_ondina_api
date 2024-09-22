531d599206377b8fe7bda56f4c68521e4c2a4d9e
Rafael Barroso <r.barroso@pimenta.group>
2024-09-21 22:58:41 -0300

    Hotfix: fix get users route

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 from rest_framework_simplejwt import views as jwt_views
-
+from uuid import UUID
 urlpatterns = [
     path("register", views.UserView.as_view()),
-    path("<int:pk>", views.UserDetailView.as_view()),
+    path("<uuid:pk>", views.UserDetailView.as_view()),
     path("login", jwt_views.TokenObtainPairView.as_view()),
 ]
