6453df93afb05ca2a656e92c6a6c93a075f68252
Rafael Barroso <r.barroso@pimenta.group>
2024-09-21 22:59:09 -0300

    Fix: fix unused imports

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from rest_framework_simplejwt import views as jwt_views
-from uuid import UUID
+
 urlpatterns = [
     path("register", views.UserView.as_view()),
     path("<uuid:pk>", views.UserDetailView.as_view()),
