4c38dd135bb17984e6ac47ad48e43f76644724b1
Rafael Barroso <r.barroso@pimenta.group>
2024-09-21 22:33:13 -0300

    Hotfix: allow cors to frontend

--- certificados_ondina_backend/settings.py
+++ certificados_ondina_backend/settings.py
@@ -41,6 +41,7 @@ DJANGO_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "drf_spectacular",
+    "corsheaders",
 ]
 
 THIRD_PARTY_APPS = [
@@ -65,6 +66,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  
+    "https://seu-dominio.com",  # adicione outros domínios permitidos
 ]
 
 ROOT_URLCONF = "certificados_ondina_backend.urls"
